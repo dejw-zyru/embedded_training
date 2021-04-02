81dd8dcee72ccd85cfed096c182470751af64858
dejw-zyru <d.zyrek94@gmail.com>
2021-04-02 17:40:34 +0200

    working from home setup

--- BATTERY_DRIVER_REV1/src/main.c
+++ BATTERY_DRIVER_REV1/src/main.c
@@ -8,7 +8,7 @@
   ******************************************************************************
 */
 
-#include <string.h>
+//#include <string.h>
 #include "stm32f1xx.h"
 #include "GPIO.h"
 #include "ADC_CONFIG.h"
@@ -18,14 +18,11 @@
 #include "BUTTON_SYSTEM_START.h"
 #include "DMA_CONFIG.h"
 
-
 #define	BUTTON_SYSTEM_START_OFF
 
 uint16_t adcValue[ADC_CHANNELS];
 
 UART_HandleTypeDef uart;
-
-
 
 void send_char(char c)
 {
@@ -40,14 +37,13 @@ int __io_putchar(int ch)
 	return ch;
 }
 
-
-
 int main(void)
 {
 
 	SystemCoreClock = 8000000; //taktowanie8MHz
 	HAL_Init();
 	ClockConfig();
+
 	//start systemu dzieki zwarciu MOSFETA-N za pomoca przycisku
 	#ifdef BUTTON_SYSTEM_START_ON
 		SystemStart();
@@ -64,29 +60,19 @@ int main(void)
 	while(1){
 
 		printf("Hello world!%d\n",i);
-
-
 		printf("\n\n\nSTM32 supply voltage is = %d convert: (%.1eV)\r\n", adcValue[0], 2 * adcValue[0] * 3.3f / 4096.0f);
-
-
 		printf("Input voltage is = %d convert:(%.1eV)\r\n", adcValue[1], 10 * adcValue[1] * 3.3f / 4096.0f);
-
-
 		printf("Reference voltage is = %d convert:(%.1eV)\r\n\n\n\n", adcValue[2],  adcValue[2] * 3.3f / 4096.0f);
+
 		HAL_Delay(2000);
 		i++;
-
 
 		if (i==1){
 			HAL_GPIO_WritePin(GPIOB, PMOS_STEP, GPIO_PIN_SET);
 			HAL_GPIO_WritePin(GPIOC, PMOS_LOGIC, GPIO_PIN_SET);
 			HAL_GPIO_WritePin(GPIOC, PMOS_DCDC, GPIO_PIN_SET);
-
 		}
-
 	}
-
-
 }
 
 
