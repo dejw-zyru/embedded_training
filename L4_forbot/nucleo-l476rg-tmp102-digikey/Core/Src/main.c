fde9858cf29eae06c1e428854a40d0d4fb7e23ad
dejw-zyru <d.zyrek94@gmail.com>
2022-07-31 16:33:56 +0200

    tmp102 is working good i2c code

--- L4_forbot/nucleo-l476rg-tmp102-digikey/Core/Src/main.c
+++ L4_forbot/nucleo-l476rg-tmp102-digikey/Core/Src/main.c
@@ -22,6 +22,7 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include <string.h>
+#include <stdio.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -44,7 +45,8 @@ I2C_HandleTypeDef hi2c1;
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
-
+static const uint8_t TMP102_ADDR = 0x48 <<1;
+static const uint8_t REG_TEMP = 0x00;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -68,7 +70,11 @@ static void MX_I2C1_Init(void);
 int main(void)
 {
   /* USER CODE BEGIN 1 */
+	HAL_StatusTypeDef ret;
 	uint8_t buf[12];
+	int16_t val; //odczytana wartosc z tmp102
+	float temp_c; //konwersja
+
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -99,8 +105,45 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-	  strcpy( (char*)buf, "Hello!\r\n" );
+	  //Tell TMP102 that we want to read from teh temp register Send first byte
+	  buf[0] = REG_TEMP;
+	  ret = HAL_I2C_Master_Transmit(&hi2c1, TMP102_ADDR, buf, 1, HAL_MAX_DELAY); //dlaczego wysylamy tutaj buf chyba domyslnie jest tak ze wysyl sie adres najpierw a pozniej dane
+	  if (  ret != HAL_OK){  					//tutaj obsluga bledu if OK idz dalej
+		  strcpy( (char*)buf, "Error Tx\r\n" );
+	  } else {
+
+		  //Read 2 bytes from the temp register
+		  ret = HAL_I2C_Master_Receive(&hi2c1, TMP102_ADDR, buf, 2, HAL_MAX_DELAY);
+		  if (  ret != HAL_OK){
+			  strcpy( (char*)buf, "Error Rx!\r\n" );
+		  } else {
+
+			  //Combine the bytes
+			  val = ((int16_t)buf[0] << 4) | (buf[1] >> 4);
+
+			  //Convert to 2's complement, since temperature can be negative
+			  if ( val > 0x7FF ){
+				  val |= 0xF000;
+			  }
+
+			  //Convert to float temperature value (Celsius)
+			  temp_c = val * 0.0625;
+
+			  //Convert temperature to decimal format
+			  temp_c *= 100;
+			  sprintf((char*)buf,
+					  "%u.%02u C\r\n",
+					  ((unsigned int)temp_c / 100),
+					  ((unsigned int)temp_c % 100));
+		  }
+
+
+	  }
+
+	  //Send out buffer (temperature or error message)
 	  HAL_UART_Transmit(&huart2, buf, strlen((char*)buf), HAL_MAX_DELAY);
+
+	  //Wait
 	  HAL_Delay(500);
     /* USER CODE END WHILE */
 
